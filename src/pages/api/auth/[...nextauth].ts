53712e05d01220beec4212653353012ee4812a45
Jakub Stündl <jakub.stundl@gmail.com>
2022-12-15 17:28:16 +0100

    credentials semi working

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,14 +4,19 @@ import DiscordProvider from "next-auth/providers/discord";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
+import CredentialsProvider from "next-auth/providers/credentials";
 
+
+import jwt, { Secret } from 'jsonwebtoken';
+import * as dotenv from "dotenv";
+import * as crypto from "crypto";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
     session({ session, user }) {
       if (session.user) {
-        session.user.id = user.id;
+        //session.user.id = user.id;
       }
       return session;
     },
@@ -23,8 +28,39 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
-   
+    CredentialsProvider({
+      type: "credentials",
+      id: "domain-login",
+      name: "Domain Account",
+      async authorize(credentials: any, req: any) {
+        const payload = {
+          email: credentials.email //|| "no password",
+        };
+        console.log("********" + payload.email);
+        
+          const user = await prisma.user.findUnique({
+          where: { email: payload.email },
+        }); 
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+      credentials: {
+        username: { label: "Username", type: "text ", placeholder: "jsmith" },
+        email: { label: "Password", type: "text" },
+      },
+    }),
   ],
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    // Set to jwt in order to CredentialsProvider works properly
+    strategy: 'jwt'
+  }
+  
+  
 };
 
 export default NextAuth(authOptions);
