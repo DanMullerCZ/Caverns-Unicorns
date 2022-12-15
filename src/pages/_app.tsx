6041d5682fd5591915cb3c51cb9da8c001c7b560
Jakub Stündl <jakub.stundl@gmail.com>
2022-12-15 13:41:13 +0100

    blankpage for welcome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
+
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
@@ -10,6 +11,8 @@ const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+ 
+  
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,9 +2,9 @@ import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
+
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -23,7 +23,7 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
-    // ...add more providers here
+   
   ],
 };
 
