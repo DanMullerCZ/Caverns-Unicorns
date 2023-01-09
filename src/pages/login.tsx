a1e04f329118217b804915f3acbc360695d142f5
Lukas <luk.kolar@seznam.cz>
2023-01-09 17:01:42 +0100

    refactored useState to useRef, slighly updated register page to fit style of main page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -12,43 +12,35 @@ import VideoBackground from 'components/VideoBackground';
 
 const Signin = () => {
   const router = useRouter();
-  const [auth, setAuth] = useState({ email: '', password: '' });
-  const emailInput = useRef<HTMLInputElement>(null)
-  const handleChange = (ev: React.FormEvent<EventTarget>) => {
-    const target: HTMLInputElement = ev.target as HTMLInputElement;
-    setAuth({ ...auth, [target.name]: target.value });
-  };
+  const input = useRef<HTMLFormElement>(null);
 
   return (
     <>
       <section>
-        <VideoBackground/>
+        <VideoBackground />
         <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
           <div className="space-y-5 rounded-xl bg-white p-10 drop-shadow-lg">
             <h1 className="text-center text-3xl">Sign In</h1>
-            <div className="flex flex-col space-y-2">
+            <form ref={input} className="flex flex-col space-y-2">
               <label className="text-sm font-light" htmlFor="email">
                 Email
               </label>
               <input
                 className="w-96 rounded-md border border-slate-400 px-3 py-2"
-                onChange={handleChange}
                 type="email"
                 name="email"
                 id="email"
               />
-
               <label className="text-sm font-light" htmlFor="password">
                 Password
               </label>
               <input
                 className="w-96 rounded-md border border-slate-400 bg-[url('/img/hero-pattern.svg')] px-3 py-2"
-                onChange={handleChange}
                 type="password"
                 name="password"
                 id="password"
               />
-            </div>
+            </form>
 
             <p className="text-right">
               <a
@@ -62,17 +54,25 @@ const Signin = () => {
               className="w-full rounded-md bg-blue-600 px-10 py-2 text-white
             duration-300 ease-in hover:bg-blue-500 hover:drop-shadow-md"
               onClick={() => {
-                signIn('credentials', { ...auth, redirect: false }).then(
-                  (response) => {
+                if (input.current) {
+                  const email = input.current.elements[0] as HTMLInputElement;
+                  const password = input.current
+                    .elements[1] as HTMLInputElement;
+                  signIn('credentials', {
+                    email: email.value,
+                    password: password.value,
+                    redirect: false,
+                  }).then((response) => {
                     if (response?.error) {
                       console.log(
-                        'Credentials were wrong or Email wasnt verified', response
+                        'Credentials were wrong or Email wasnt verified',
+                        response,
                       );
                     } else {
                       router.push(process.env.HOST || '/');
                     }
-                  },
-                );
+                  });
+                }
               }}
             >
               Login with credentials
