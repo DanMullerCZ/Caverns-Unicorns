bb5851bf5718eb46695f005069e3b0ac67e2ed1b
Lukas <luk.kolar@seznam.cz>
2023-01-14 21:30:45 +0100

    user mistakes handling, error handling, show correct message from BE need to be done

--- src/components/userSettings/ChangePasswordOption.tsx
+++ src/components/userSettings/ChangePasswordOption.tsx
@@ -1,46 +1,24 @@
 import { NextPage } from "next";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { trpc } from "utils/trpc";
 
-type passwordState = true | false | 'notprovided'
 
 const ChangePasswordOption: NextPage = () => {
     const passwordInput = useRef<HTMLFormElement>(null);
     const userPassword = trpc.userSettings.passwordCheck.useMutation()
-    const newPassword = trpc.userSettings.changePassword.useMutation()
-    let passwordStatus = ''
+    let [passwordStatus, setPasswordStatus] = useState('')
+
     const handleClick = (ev: React.FormEvent<EventTarget>) => {
-        if(passwordInput.current?.elements[1].value === passwordInput.current?.elements[2].value && passwordInput.current?.elements[0].value.length ){
+        if(passwordInput.current?.elements[1].value === passwordInput.current?.elements[2].value && passwordInput.current?.elements[0].value.length && passwordInput.current?.elements[2].value.length){
             const currentPassword: any = { currentPassword:  passwordInput.current?.elements[0].value, newPassword: passwordInput.current?.elements[1].value }
                 ev.preventDefault()
-                    passwordStatus = 'password need to be longer'
-                    userPassword.mutate(currentPassword)
-                    changePassword()
+                userPassword.mutate(currentPassword)
+                setPasswordStatus(`Succesfully changed password!`)
         } else {
-            passwordStatus = 'provided invalid new passwords'
+            ev.preventDefault()
+            setPasswordStatus( `incorrect inputs backeng msg: ${userPassword.data?.toString()}`)
         }
-            // if( userPassword.data === true ){
-            //     console.log(userPassword.data);
-
-            //     if(passwordInput.current?.elements[1].value === passwordInput.current?.elements[2].value){
-            //         console.log('correct password');
-            //         newPassword.mutate(passwordInput.current?.elements[1].value)
-            //         console.log('succes');              
-            //     } else {
-            //         console.log('new password and confirmation must be the same');             
-            //     }
-            // } else {
-            //     console.log('wrong password');    
-            // }
     }
-
-    const changePassword = () => {
-      
-    }
-
-   
-
-
     return  (
         <>  <form ref={passwordInput} onSubmit={handleClick} className='border '>
                 <input  type="text" placeholder="current password"/>
--- src/server/routers/userSettings.ts
+++ src/server/routers/userSettings.ts
@@ -15,9 +15,6 @@ export const userSettRouter = router({
         }))
         .mutation(async({input, ctx }) => {      
             try {
-                if( input.currentPassword === '' ){
-                    return 'Current password required'
-                }
                 const userPass: any = await prisma.user.findFirst({
                     where: { id: ctx.session?.user?.id } ,
                     select: { password: true }
@@ -37,29 +34,10 @@ export const userSettRouter = router({
                     })
                     return `Succesfully changed password ${newPassword}`
                 } else {
-                    return 'something went wrong :('
+                    return 'wrong password'
                 }
             } catch (e) {
                 return `Cannot acces data from database, error:${e}`
             }
-        }),
-    changePassword: publicProcedure
-            .input(z.string())
-            .mutation(async({ input, ctx }) => {
-                try{
-                    const hashedNewPass = hashToken(input)
-                    const newPassword: any = await prisma.user.update({
-                        where: { 
-                            id: ctx.session?.user?.id 
-                        },
-                        data: {
-                            password: hashedNewPass
-                        }  
-                    })
-                    console.log('succes');                   
-                    return newPassword
-                } catch(e){
-                    return 'Cannot update database in change password procedure'
-                }
         })
-    })
+    })        
