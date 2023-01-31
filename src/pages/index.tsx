35fb312a89a856612a6b8fd4d15dcf21455f8460
Daniel Műller <daniel.muller1918@seznam.cz>
2023-01-31 17:31:56 +0100

    upgraded homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,24 @@ import NavigationBar from 'components/NavigationBar';
 import Header from 'components/general/Header';
 
 const Home: NextPage = () => {
+  const { data: sessionData } = useSession();
+
   return (
     <>
       <Header title="Home Page" />
       <VideoBackground />
       <NavigationBar />
       <main className="goldeffect gold mx-auto flex flex-col items-center justify-center gap-10 px-6 py-8 font-LOTR md:h-screen">
-        <h1 test-id="succes delete" className=" goldeffect gold text-[100px]">
+        <h1 test-id="succes delete" className=" goldeffect gold text-6xl">
           Welcome to Caverns & Unicorns
         </h1>
-        <AuthShowcase />
+        {sessionData && (
+        <Link className='goldeffect gold text-4xl mt-12' href={'/lobby'}>
+          Start Game!
+        </Link>
+      )}
       </main>
+      <AuthShowcase />
     </>
   );
 };
@@ -27,11 +34,23 @@ const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
 
   return (
-    <div className="text-[40px]">
-      <p>{sessionData && <span>Logged in as {sessionData.user?.name}</span>}</p>
+    <div className="text-3xl mr-10 mt-3 font-LOTR" 
+      style={{
+        position: 'absolute',
+        top: '0',
+        right: '0',
+        display: 'flex',
+        justifyContent: `${sessionData?.user ? 'center' : 'space-around'}`,
+        alignItems: 'center',
+        flexDirection:`${sessionData?.user ? 'column' : 'row'}`,
+        width: '20vw',
+
+      }}
+    >
+      <p className="gold text-center m-auto">{sessionData && <span>Logged in as {sessionData.user?.name}</span>}</p>
 
       <button
-        className="goldeffect mr-12 "
+        className="goldeffect gold text-center m-auto"
         onClick={
           sessionData
             ? () => {
@@ -43,7 +62,7 @@ const AuthShowcase: React.FC = () => {
         {sessionData ? 'Log Out' : 'Log In'}
       </button>
       {!sessionData && (
-        <Link className="goldeffect" href={'/registration'}>
+        <Link className='goldeffect gold' href={'/registration'}>
           Register
         </Link>
       )}
